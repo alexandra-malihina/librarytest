703f0c3f4d6e6bbc48c1c19b8c5aea25bde47bcb
Alexandra M <work.mal@mail.ru>
2024-01-19 09:15:16 +0000

    first version

---
+++ src/Finder.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AlexandraMalihina\Librarytest;
+
+
+class Finder
+{
+    public function whoIsAwesome(): string
+    {
+        return 'you <3';
+    }
+}
